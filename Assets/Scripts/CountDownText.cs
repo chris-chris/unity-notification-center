85ea6e9c6bbf18c9e8af8f6cd54db236c2e6231b
Chris <sjhshy@gmail.com>
2017-07-01 12:30:08 +0900

    no message

---
+++ Assets/Scripts/CountDownText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownText : MonoBehaviour {
+
+	int count = 20;
+
+	Text text;
+
+	// Use this for initialization
+	void Start () {
+
+		text = GetComponent<Text> ();
+
+		NotificationCenter.Instance.Add ("CountDown", OnCountDown);
+	}
+
+	void OnCountDown(){
+		count --;
+		text.text = count.ToString();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		while (true) {
+			yield return new WaitForSecondsRealtime (1f);
+			NotificationCenter.Instance.Notify ("CountDown");
+		}
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
